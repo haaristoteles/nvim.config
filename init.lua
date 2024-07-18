87f49db12ec94e60bd614b5042879632bffe7208
Jens Doveren <doveren@informatik.rwth-aachen.de>
2024-07-18 16:28:10 +0200

    removed next_ls

--- init.lua
+++ init.lua
@@ -607,7 +607,6 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        'nextls',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
