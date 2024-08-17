84be519e77b4734fd02b86e6e1fe8f2557ac297e
Jens Doveren <doveren@informatik.rwth-aachen.de>
2024-08-17 20:09:27 +0200

    swapped ; and ,

--- init.lua
+++ init.lua
@@ -4,6 +4,9 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.keymap.set({ 'n' }, ';', ',', { remap = false })
+vim.keymap.set({ 'n' }, ',', ';', { remap = false })
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
