32355dc1b33dd5c42b132e366f356b32680a0b36
Jens Doveren <doveren@informatik.rwth-aachen.de>
2024-08-17 20:11:40 +0200

    extended swap to visual mode

--- init.lua
+++ init.lua
@@ -4,8 +4,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.keymap.set({ 'n' }, ';', ',', { remap = false })
-vim.keymap.set({ 'n' }, ',', ';', { remap = false })
+vim.keymap.set({ 'n', 'v' }, ';', ',', { remap = false })
+vim.keymap.set({ 'n', 'v' }, ',', ';', { remap = false })
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
