609e1a15e78a06f8436ee3a873c868c308f2b4a6
haaristoteles <18413475+haaristoteles@users.noreply.github.com>
2024-11-04 14:49:15 +0100

    configured surround plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -34,6 +34,17 @@ return {
     config = function()
       require('nvim-surround').setup {
         -- Configuration here, or leave empty to use defaults
+        keymaps = {
+          normal = 'ys',
+          normal_cur = 'yss',
+          normal_line = 'yS',
+          normal_cur_line = 'ySS',
+          visual = 'S',
+          visual_line = 'gS',
+          delete = 'ds',
+          change = 'cs',
+          change_line = 'cS',
+        },
       }
     end,
   },
